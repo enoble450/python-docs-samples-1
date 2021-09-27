f066ea8f2663055c1fbc048a170d04e90516c7e6
enoble450 <46541338+enoble450@users.noreply.github.com>
2021-09-27 12:06:32 -0700

    A

--- main.py
+++ main.py
@@ -1,26 +1,18 @@
-# Copyright 2018 Google LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-# [START gae_python38_app]
-# [START gae_python3_app]
 from flask import Flask
 import os
-
-# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
-# called `app` in `main.py`.
+from google.cloud import datastore
 app = Flask(__name__)
-
+dataclient = datastore.Client()
+def addVisitor():
+	ent = dataclient.key('data','visitors')
+	total = dataclient.get(key=ent)
+	if total:
+		total['total'] += 1
+		dataclient.put(total)
+	else:
+		total = datastore.Entity(key=ent)
+		total['total'] = 0
+		dataclient.put(total)
 @app.route('/')
 def hello():
 	return 'Hello world!'
@@ -31,12 +23,19 @@ def vers():
 def getid():
 	instanceid = os.getenv('GAE_INSTANCE')
 	return str(instanceid)
-	
-
+@app.route('/version-id')
+def getversionid():
+	addVisitor()
+	versionid = os.getenv('GAE_VERSION')
+	return str(versionid)
+@app.route('/visitors')
+def getVisitor():
+	addVisitor()
+	ent = dataclient.key('data','visitors')
+	total = dataclient.get(key=ent)
+	if total:
+		return 'Total visitors: ' + str(total['total'])
+	else:
+		return 'Total Broke!'
 if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. You
-    # can configure startup instructions by adding `entrypoint` to app.yaml.
     app.run(host='127.0.0.1', port=8080, debug=True)
-# [END gae_python3_app]
-# [END gae_python38_app]
