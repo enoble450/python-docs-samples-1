c9627604d4d92f4b12e212b1681e279880566f6f
enoble450 <46541338+enoble450@users.noreply.github.com>
2021-10-04 12:07:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -78,6 +78,6 @@ def submit_post():
 			dataclient.put(posts)
 		return redirect('/')
 	else:
-		return redirect('/'
+		return redirect('/')
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
